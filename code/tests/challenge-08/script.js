83df72c500efbb78c2d5a65678b77b2c204b5427
WENDER JOSE BARBOSA SANTOS <wenblack012@gmail.com>
2022-05-09 09:00:04 -0300

    Challenge Final

---
+++ code/tests/challenge-08/script.js
@@ -0,0 +1,12 @@
+let numberOne = prompt("Digite o número 1");
+let numberTwo = prompt("Digite o número 2");
+
+let sum = Number(numberOne) + Number(numberTwo);
+let sub = Number(numberOne) - numberTwo;
+let multi = Number(numberOne) * Number(numberTwo);
+let div = Number(numberOne) % Number(numberTwo);
+
+alert("A soma dos dois números é " + sum);
+alert("A subtração dos dois números é " + sub);
+alert("A multiplicação dos dois números é " + multi);
+alert("O restante da divisão dos números é " + div);
