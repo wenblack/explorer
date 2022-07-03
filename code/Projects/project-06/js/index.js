fac051eb50487651c08ffc1bc90339c278885197
Wender Barbosa <wenblack012@gmail.com>
2022-07-03 13:48:29 -0300

    Start ES6 modules

--- code/Projects/project-06/js/index.js
+++ code/Projects/project-06/js/index.js
@@ -1,26 +1,16 @@
+import resetControls from "./controls.js";
+import timer from "./timer.js";
+
 const buttonPlay = document.querySelector(".play");
 const buttonPause = document.querySelector(".pause");
 const buttonStop = document.querySelector(".stop");
 const buttonSet = document.querySelector(".set");
 const buttonSoundOn = document.querySelector(".sound-on");
 const buttonSoundOff = document.querySelector(".sound-off");
-let minutes;
 const minutesDisplay = document.querySelector(".minutes");
 const secondsDisplay = document.querySelector(".seconds");
-
-function countdown() {
-  setTimeout(function () {
-    let seconds = Number(secondsDisplay.textContent);
-
-    if (seconds <= 0) {
-      seconds = 60;
-    }
-
-    secondsDisplay.textContent = seconds - 1;
-
-    countdown();
-  }, 1000);
-}
+let minutes = Number(minutesDisplay.textContent);
+let timerTimeOut;
 
 buttonPlay.addEventListener("click", function () {
   buttonPlay.classList.add("hide");
@@ -34,15 +24,12 @@ buttonPlay.addEventListener("click", function () {
 buttonPause.addEventListener("click", function () {
   buttonPause.classList.add("hide");
   buttonPlay.classList.remove("hide");
+  clearTimeout(timerTimeOut);
 });
 
 buttonStop.addEventListener("click", function () {
-  buttonPlay.classList.remove("hide");
-  buttonPause.classList.add("hide");
-  buttonSet.classList.remove("hide");
-  buttonStop.classList.add("hide");
-
-  stopCount();
+  resetControls();
+  resetTimer();
 });
 
 buttonSoundOff.addEventListener("click", function () {
@@ -56,6 +43,12 @@ buttonSoundOn.addEventListener("click", function () {
 });
 
 buttonSet.addEventListener("click", function () {
-  minutes = prompt("Quantos minutos?");
-  minutesDisplay.textContent = minutes;
+  let newMinutes = prompt("Quantos minutos?");
+  if (!newMinutes) {
+    resetTimer();
+    return;
+  }
+
+  minutes = newMinutes;
+  updateTimerDisplay(minutes, 0);
 });
